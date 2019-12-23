d18ac023adf5b00615222c7d3c435c14611e6c21
jkrolik1 <56318896+jkrolik1@users.noreply.github.com>
2019-12-23 12:00:58 +0100

    Klasa player2

--- Player.java
+++ Player.java
@@ -1,12 +1,23 @@
-package Lesson2;
-
 import java.util.Random;
 
 public class Player {
-    private Random dice = new Random();             //pole klasy
+    private Random dice = new Random();         //pole klasy
     private String name;
-    public void setName(String name){               //setter!!!!!!
-        if(name!=null){
+  //private String name="Anonim";               może zamiast konstruktora bezparametrowego wtedy public Player() {}
+    public Player(){
+        //this.name="Anonim";                   //raczej nie!!!
+        setName("Anonim");
+    }                                           //ta sama nazwa funkcji np: dla konstruktora to PRZECIĄŻENIE
+    public Player(String name){
+        //this.name=name;                       //raczej nie!!!
+        setName(name);
+    }
+    public void setName(String name){           //setter!!!!!!
+        if(name!=null && !(name.isEmpty())){    //muszą być oba prawdziwe (Ważna kolejność!!!)
+      //if(name!=null && name.length!=0)          
+      //if(name!=null && !(name.equals("")))          
+      //if(name!=null && !("".equals(name)))         
+      //if(name!=null && name!="")              //nie!!!
             this.name=name;
         }
         else{
