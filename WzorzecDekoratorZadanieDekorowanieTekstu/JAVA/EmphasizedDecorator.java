adf3492e76b6dda2eff28d999be069981fb3263a
jkrolik1 <56318896+jkrolik1@users.noreply.github.com>
2020-03-22 19:09:23 +0100

    Rebuild

--- WzorzecDekoratorZadanieDekorowanieTekstu/JAVA/EmphasizedDecorator.java
+++
@@ -1,9 +0,0 @@
-public class EmphasizedDecorator extends TextDecorator{
-    public EmphasizedDecorator(PlainText plainText) {
-        super(plainText);
-    }
-    public void write(){
-        super.write();
-        System.out.print("</em>");
-    }
-}
