a0e3fe3a12eed331ad6246ca27649ce1fcedc95c
jkrolik1 <56318896+jkrolik1@users.noreply.github.com>
2020-03-22 19:11:33 +0100

    Rebuild

--- WzorzecDekoratorZadanieDekorowanieTekstu/JAVA/TextDecorator.java
+++
@@ -1,8 +0,0 @@
-abstract class TextDecorator extends PlainText{
-    protected PlainText plainText;
-    public TextDecorator(PlainText plainText)  { this.plainText = plainText; }
-    @Override
-    public void write(){
-        plainText.write();
-    }
-}
