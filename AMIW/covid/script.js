09050d6a2f0863fe02fab69f1a0d361f481e61de
jkrolik1 <56318896+jkrolik1@users.noreply.github.com>
2020-11-08 18:12:20 +0100

    Chart

--- AMIW/covid/script.js
+++ AMIW/covid/script.js
@@ -1,24 +1,49 @@
 fetch('https://api.covid19api.com/country/poland/status/confirmed/live?from=2020-09-01T00:00:00Z&to=2020-11-01T00:00:00Z%27')
 	.then(response => response.json())
 	.then(data => {
-		let cases = [], day = [];
+		let cases = [], day = [], colors = [];
 		var prevDay = data[data.length-1].Cases;
+		var c = 0, total = 0;
 		for(var i = data.length-2; i>=1; i--){
-			cases.push(parseInt(prevDay - data[i].Cases));
+			c = (parseInt(prevDay - data[i].Cases));
+			cases.push(c);
+			total += c;
 			day.push(new Date(data[i+1].Date).toLocaleDateString());
 			prevDay = data[i].Cases;
 		}
 
+		var avg = (total/cases.length);
+
+		for(i in cases){
+			if(cases[i] >= avg)
+				colors.push("#F08080");
+			else 
+				colors.push("#2E8B57");
+		}
+
 		function loadChart() {
-			var myChart = new Chart(document.getElementById("chart"), {
+			Chart.defaults.global.defaultFontColor = '#EAEAEA';
+			Chart.defaults.global.defaultFontSize = 17;
+			Chart.defaults.global.borderColor = 'blue';
+			var ch = new Chart(document.getElementById("chart"), {
 				type: 'horizontalBar',
 				data: {
 					labels: day,
 					datasets: [{
-						label: 'Cases',
+						label: 'Liczba przypadków',
 						data: cases,
+						backgroundColor: colors
 					}]
 				},
+				options: {
+					legend: { display: false },
+					title: {
+						display: true,
+						text: 'Przypadki koronawirusa w Polsce',
+						fontSize: 30,
+						padding: 25
+					}
+				}
 			});
 		}
 		loadChart();
