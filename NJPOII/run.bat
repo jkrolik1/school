60350c3c4f1dc59aae74955e6c9e80c1dc26052d
jkrolik1 <56318896+jkrolik1@users.noreply.github.com>
2020-11-15 18:44:54 +0100

    NewTasks

--- NJPOII/run.bat
+++ NJPOII/run.bat
@@ -1,1 +1,1 @@
-docker run -it -p 8888:8888 -v d:\Dokumenty\GitHub\school\NJPOII\jupyter:/home/jovyan/work jupyter/scipy-notebook
+docker run -it -p 8888:8888 -v d:\Dokumenty\GitHub\school\NJPOII:/home/jovyan/work jupyter/scipy-notebook
