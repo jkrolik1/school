d2f5248f14cf48ce6057343d8b557ad7ddc89eb8
jkrolik1 <56318896+jkrolik1@users.noreply.github.com>
2020-12-03 21:37:45 +0100

    refresh

--- AMIW/ng2date/test-app/src/app/app.component.ts
+++ AMIW/ng2date/test-app/src/app/app.component.ts
@@ -11,12 +11,9 @@ import { TodoService } from './todo.service';
 
 export class AppComponent {
   
-  private todoForm: FormGroup;
-  private todos: { title: string; date: string; }[];
-  private todosDone: { title: string; date: string; }[];
-
-  style2 = {'text-decoration': 'line-through',
-            'color': 'rgb(150, 150, 150)'};
+  todoForm: FormGroup;
+  todos: { title: string; date: string; }[];
+  todosDone: { title: string; date: string; }[];
 
   constructor(
     private todoService: TodoService,
@@ -38,18 +35,6 @@ export class AppComponent {
     this.todoForm.reset();
   }
 
-  done(index: number): void{
-    this.todoService.done(index);
-    this.todos = this.todoService.findAll();
-    this.todosDone = this.todoService.findAll2();
-  }
-
-  undone(index: number): void{
-    this.todoService.undone(index);
-    this.todos = this.todoService.findAll();
-    this.todosDone = this.todoService.findAll2();
-  }
-
  removeAll(): void{
     var result = confirm("Czy jesteś pewien?");
     if(result){
@@ -58,32 +43,5 @@ export class AppComponent {
       this.todosDone = this.todoService.findAll2();
     }
   } 
-
-  delete(index: number): void{
-    var result = confirm("Czy jesteś pewien?");
-    if(result){
-      this.todoService.delete(index);
-      this.todos = this.todoService.findAll();
-      this.todosDone = this.todoService.findAll2();
-    }
-  }
-  delete2(index: number): void{
-    var result = confirm("Czy jesteś pewien?");
-    if(result){
-      this.todoService.delete2(index);
-      this.todos = this.todoService.findAll();
-      this.todosDone = this.todoService.findAll2();
-    }
-  }
-
-  goUp(index: number): void{
-    if(index == 0){
-      var result = confirm("Jestem już na górze.");
-    }
-    this.todoService.goUp(index);
-    this.todos = this.todoService.findAll();
-    this.todosDone = this.todoService.findAll2();
-  }
-
 }
 
--- AMIW/ng2date/test-app/src/app/item/item.component.ts
+++ AMIW/ng2date/test-app/src/app/item/item.component.ts
@@ -9,9 +9,12 @@ import { TodoService } from '../todo.service';
 })
 export class ItemComponent implements OnInit {
 
-  private todoForm: FormGroup;
-  private todos: { title: string; date: string; }[];
-  private todosDone: { title: string; date: string; }[];
+  todoForm: FormGroup;
+  todos: { title: string; date: string; }[];
+  todosDone: { title: string; date: string; }[];
+
+  style2 = {'text-decoration': 'line-through',
+            'color': 'rgb(150, 150, 150)'};
 
   constructor(
     private todoService: TodoService,
@@ -19,11 +22,47 @@ export class ItemComponent implements OnInit {
   ) { }
 
   ngOnInit(){
-    this.todoForm = this.FormBuilder.group({
-      title: ['',[Validators.required]]
-    });    
     this.todos = this.todoService.findAll();
     this.todosDone = this.todoService.findAll2();
   }
+
+  delete(index: number): void{
+    var result = confirm("Czy jesteś pewien?");
+    if(result){
+      this.todoService.delete(index);
+      this.todos = this.todoService.findAll();
+      this.todosDone = this.todoService.findAll2();
+    }
+  }
+  delete2(index: number): void{
+    var result = confirm("Czy jesteś pewien?");
+    if(result){
+      this.todoService.delete2(index);
+      this.todos = this.todoService.findAll();
+      this.todosDone = this.todoService.findAll2();
+    }
+  }
+
+  goUp(index: number): void{
+    if(index == 0){
+      var result = confirm("Jestem już na górze.");
+    }
+    this.todoService.goUp(index);
+    this.todos = this.todoService.findAll();
+    this.todosDone = this.todoService.findAll2();
+  }
+
+  done(index: number): void{
+    this.todoService.done(index);
+    this.todos = this.todoService.findAll();
+    this.todosDone = this.todoService.findAll2();
+  }
+
+  undone(index: number): void{
+    this.todoService.undone(index);
+    this.todos = this.todoService.findAll();
+    this.todosDone = this.todoService.findAll2();
+  }
+  
 
 }
