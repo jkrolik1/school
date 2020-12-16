296206c989f07bcc6ffe728acf942ee81fd70f92
jkrolik1 <56318896+jkrolik1@users.noreply.github.com>
2020-12-16 18:50:58 +0100

    ChangeProjectStartingPoint

--- AMIW/routing-app/e2e/src/app.e2e-spec.ts
+++ AMIW/routing-app/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('rApp app is running!');
+    expect(page.getTitleText()).toEqual('routing-app app is running!');
   });
 
   afterEach(async () => {
