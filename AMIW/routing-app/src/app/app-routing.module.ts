83c4c761b0d5ccbdadce367c3b04da65c057224b
jkrolik1 <56318896+jkrolik1@users.noreply.github.com>
2021-01-05 21:50:32 +0100

    Uncommited changes

--- AMIW/routing-app/src/app/app-routing.module.ts
+++ AMIW/routing-app/src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import { ThirdComponent } from './components/third/third.component';
 import { BestAllComponent } from './components/bestall/bestall.component';
 
 const routes: Routes = [
-  { path: 'the-best-today-animals', component: FirstComponent },
-  { path: 'the-best-today-blonde', component: SecondComponent },
-  { path: 'the-best-today-knockknock', component: ThirdComponent },
-  { path: 'the-best-today', component: BestAllComponent },
+  { path: 'first-component', component: FirstComponent },
+  { path: 'second-component', component: SecondComponent },
+  { path: 'third-component', component: ThirdComponent },
+  { path: 'bestall-component', component: BestAllComponent },
 ];
 
 @NgModule({
