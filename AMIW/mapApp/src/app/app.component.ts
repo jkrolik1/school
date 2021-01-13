c0279bb6e0d478fb30e589df66cdfecf2af211e5
jkrolik1 <56318896+jkrolik1@users.noreply.github.com>
2021-01-13 15:44:28 +0100

    Map app

--- AMIW/mapApp/src/app/app.component.ts
+++ AMIW/mapApp/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+declare let ol: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mapApp';
+  
+  map: any;
+
+  ngOnInit() {
+    this.map = new ol.Map({
+      "target": "map",
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([39, 50]),
+        zoom: 4
+      })
+    });
+  }
 }
+// Nowe api???
+// Nakładanie znaczników, warstwy, dymki (https://openlayers.org/en/latest/doc/quickstart.html)
+// Nałożenie zanieczyszczenia powietrza itd, śnieg, kropki
+// https://openweathermap.org/current#current_JSON
