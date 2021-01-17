e15082b4840dea402d0441306552939f057e761e
jkrolik1 <56318896+jkrolik1@users.noreply.github.com>
2021-01-17 15:10:08 +0100

    App finish

--- AMIW/mapApp/src/app/app.component.ts
+++ AMIW/mapApp/src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { MapServiceService} from '../app/services/map-service.service'
-import MousePosition from 'ol/control/MousePosition';
 import {OSM, Vector as VectorSource} from 'ol/source';
 import {Point} from 'ol/geom';
-import {createStringXY} from 'ol/coordinate';
-import {defaults as defaultControls} from 'ol/control';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
-import {useGeographic} from 'ol/proj';
 import {Feature, Map, View} from 'ol/index';
-
-useGeographic();
+import { fromLonLat } from 'ol/proj';
+import {createStringXY} from 'ol/coordinate';
+import MousePosition from 'ol/control/MousePosition';
+import {defaults as defaultControls} from 'ol/control';
 
 
 @Component({
@@ -19,46 +16,82 @@ useGeographic();
 })
 export class AppComponent {
   
-  map: any;
-  place: any = [21.012229, 52.229676];
-  point: any = new Point(this.place);
-  
-  constructor(private MapService: MapServiceService) { }
+  map: Map;
+  link: RequestInfo;
+  weather: String;
+  vectorLayer: VectorLayer;
+  mousePositionControl: MousePosition;
+  len1old: DoubleRange;
+  len2old: DoubleRange;
+  layer1: VectorLayer;
+  iconPath: String;
 
-  mousePositionControl = new MousePosition({
-    coordinateFormat: createStringXY(4),
-    projection: 'EPSG:4326',
-    // comment the following two lines to have the mouse position
-    // be placed within the map.
-    className: 'custom-mouse-position',
-    target: document.getElementById('mouse-position'),
-    undefinedHTML: '&nbsp;',
-  });
+  constructor() { }
 
-  ngOnInit() {
+  ngOnInit(){
+    this.mousePositionControl = new MousePosition({
+      coordinateFormat: createStringXY(4),
+      projection: 'EPSG:4326',
+    });
+    this.getAPI(21,52);
+    this.createMap(21,52);
+  }
+
+  newPoint(l1,l2){
+    if (this.vectorLayer)
+      this.map.removeLayer(this.vectorLayer);
+    this.map.removeLayer(this.layer1);
+    var point: any = new Point(fromLonLat([l1, l2]));
+    this.layer1 = (new VectorLayer({
+      source: new VectorSource({
+        features: [new Feature(point)],
+      }),
+    }));
+    this.map.addLayer(this.layer1);
+  }
+
+  newCenter(l1,l2){
+    this.map.getView().setCenter(fromLonLat([l1, l2]));
+  }
+
+  createMap(l1,l2) {
+    var point: any = new Point(fromLonLat([l1, l2]));
+    this.vectorLayer = new VectorLayer({
+      source: new VectorSource({
+        features: [new Feature(point)],
+      }),
+    });
     this.map = new Map({
       controls: defaultControls().extend([this.mousePositionControl]),
       layers: [
         new TileLayer({
           source: new OSM(),
         }), 
-        new VectorLayer({
-          source: new VectorSource({
-            features: [new Feature(this.point)],
-          }),
-        }),
+        this.vectorLayer,
       ],
       target: 'map',
       view: new View({
-        center: this.place,
+        center: fromLonLat([l1, l2]),
         zoom: 5,
       }),
     });
-    this.MapService.getAPI().subscribe((open) => {
-      this.point = open;
-    });
-    
-
   }
 
+  getAPI(l1,l2){
+    this.link = "http://api.openweathermap.org/data/2.5/weather?lat=" + l1 + "&lon=" + l2 + "&&APPID=3300e901023b082fd1c9ab305058add4";
+    fetch(this.link)
+    .then(response => response.json())
+    .then(data => {
+      var j = data;
+      this.weather = (j.weather[0].main) + ", ";
+      this.weather += (j.weather[0].description);
+      this.iconPath = "http://openweathermap.org/img/w/" + j.weather[0].icon + ".png";
+    });
+  }
+
+  onAdd(len1,len2){
+    this.getAPI(len1,len2);
+    this.newPoint(len1,len2);
+    this.newCenter(len1,len2);
+  }
 }
