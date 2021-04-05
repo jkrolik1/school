153b7342cc17da113e508f4b358321c262b4dce4
jkrolik1 <56318896+jkrolik1@users.noreply.github.com>
2021-04-05 18:28:17 +0200

    Query correction

--- PAW/diethelper-db/src/main/java/com/diethelper/db/diethelperdb/repository/PreparationRepository.java
+++ PAW/diethelper-db/src/main/java/com/diethelper/db/diethelperdb/repository/PreparationRepository.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 public interface PreparationRepository extends JpaRepository<Meal, Integer> {
-    //@Query("SELECT preparation.ProductproductId, product.name FROM preparation, product WHERE MealmealId = :mealId AND preparation.ProductproductId = product.productId");
+    @Query("SELECT preparation.ProductproductId, product.name FROM preparation, product WHERE MealmealId = :mealId AND preparation.ProductproductId = product.productId")
     List<Product> findByMeal(@PathVariable("mealId") int id);
 }
