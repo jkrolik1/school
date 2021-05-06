bc6ddac87a81f30b381928ca76028a1850e434b1
jkrolik1 <56318896+jkrolik1@users.noreply.github.com>
2021-05-06 17:18:46 +0200

    Add spring security

---
+++ PAW/diethelper-db/src/main/java/com/diethelper/db/diethelperdb/config/LoginCredentials.java
@@ -0,0 +1,10 @@
+package com.diethelper.db.diethelperdb.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+public class LoginCredentials {
+    private String username;
+    private String password;
+}
